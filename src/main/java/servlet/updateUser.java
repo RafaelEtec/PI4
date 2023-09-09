79f6f1714b811fd7b77d7b0bddaaa69a718d3f24
Rafael <rafaelxms@hotmail.com>
2023-09-09 14:41:06 -0300

    > implemented but to do

---
+++ src/main/java/servlet/updateUser.java
@@ -0,0 +1,9 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/updateUser")
+public class updateUser extends HttpServlet {
+
+}
