7da14be8c6cc99f6d97e71f7a9a774b39af7943b
rafael.goulart <rafaelxms@hotmail.com>
2023-10-17 14:45:55 -0300

    > updated

--- src/main/java/servlet/indexToUser.java
+++ src/main/java/servlet/indexToUser.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import dao.prDAO;
+import model.Cliente;
 import model.Produto;
 
 import javax.servlet.ServletException;
@@ -16,18 +17,27 @@ public class indexToUser extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<Produto> pianos = new prDAO().listCarrouselCardsByName("Piano");
-        List<Produto> guitarras = new prDAO().listCarrouselCardsByName("Guitarra");
-        List<Produto> flautas = new prDAO().listCarrouselCardsByName("Flauta");
-        List<Produto> violoes = new prDAO().listCarrouselCardsByName("Violao");
-        List<Produto> saxofones = new prDAO().listCarrouselCardsByName("Saxofone");
+        List<Produto> pianos = new prDAO().listCarrouselCardsByTag("PIANO");
+        List<Produto> guitarras = new prDAO().listCarrouselCardsByTag("GUITARRA");
+        List<Produto> flautas = new prDAO().listCarrouselCardsByTag("FLAUTA");
+        List<Produto> violoes = new prDAO().listCarrouselCardsByTag("VIOLAO");
+        List<Produto> saxofones = new prDAO().listCarrouselCardsByTag("SAXOFONE");
 
         req.setAttribute("pianos", pianos);
         req.setAttribute("guitarras", guitarras);
         req.setAttribute("flautas", flautas);
         req.setAttribute("violoes", violoes);
         req.setAttribute("saxofones", saxofones);
+        String status = "";
 
+        Cliente cliente = (Cliente) req.getSession().getAttribute("cliente");
+        if (cliente == null) {
+            status = "naologado";
+        } else {
+            status = "logado";
+        }
+
+        req.setAttribute("sessionStatus", status);
         req.getRequestDispatcher("Disciplina-Musical.jsp").forward(req, resp);
     }
 }
